04e726ce5abb26a4e85056e2589336203e3d5659
Anna <viirall@yandex.ru>
2020-05-16 11:56:46 +0300

    up

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-const postcss = require("postcss")
-const postcssPresetEnv = require('postcss-preset-env');
-const atImport = require("postcss-import")
-
-postcss([
-  atImport({}),
-  postcssPresetEnv({ browsers: ['last 2 versions', '> 5%'] })
-]);
